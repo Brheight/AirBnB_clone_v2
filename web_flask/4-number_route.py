7db68a168227b27c9794d64fb4d44212dac262e0
Bright <brightoadewole@gmail.com>
2024-02-29 10:11:19 -0800

    stp

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 """
-This module starts a Flask web application with two routes.
+starts a Flask web application
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Display "Hello HBNB!" on the root route.
-    """
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Display "HBNB" on the /hbnb route.
-    """
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
@@ -35,5 +31,5 @@ def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
