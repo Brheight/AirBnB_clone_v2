4aa38c8ee5608976a25139881f5056a2eaaf6c85
Bright <brightoadewole@gmail.com>
2024-02-29 10:32:30 -0800

    lsf

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 """
-This module starts a Flask web application with two routes.
+starts a Flask web application
 """
 
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Display "Hello HBNB!" on the root route.
-    """
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Display "HBNB" on the /hbnb route.
-    """
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
@@ -57,5 +53,6 @@ def numbersandevenness(n):
         evenness = 'odd'
     return render_template('6-number_odd_or_even.html', n=n,
                            evenness=evenness)
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
